bdb604d878093d02959d383f2019c3820ce4bf0d
gustavo.ruske <gustavo.ruske@LAB-B121-30.univille.br>
2017-03-20 21:25:26 -0300

    20-03 21:25

--- Bovesa/src/com/bovespa/StockManager.java
+++ Bovesa/src/com/bovespa/StockManager.java
@@ -6,15 +6,25 @@ import java.nio.file.Paths;
 import java.util.stream.Stream;
 
 public class StockManager {
-	
-	public Stock[] loadStock( String fileName) throws IOException{
-		
+	private int i = 1;
+
+	public Stock[] loadStock(String fileName) throws IOException {
+
 		Stream<String> lines = Files.lines(Paths.get(fileName));
-		
+		Stock[] stocks = new Stock[20];
+
 		lines.forEach(s -> {
-			System.out.println(s);
+			if (s.startsWith("01")) {
+				
+				Stock stock = new Stock();
+				stock.setCode(s.substring(12, 24).trim());
+				//stock.setDate(s.substring(2, 10).trim());
+				stocks[i++] = stock;
+				System.out.println(stock.getCode());
+			}
+
 		});
-		
+
 		return null;
 	}
 }
